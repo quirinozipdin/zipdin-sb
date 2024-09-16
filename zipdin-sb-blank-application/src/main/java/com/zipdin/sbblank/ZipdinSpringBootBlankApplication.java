026e37d28bd45af8f3c619d298722459e5dc63d2
eduquirino08 <eduardo.quirino@zipdin.com.br>
2024-09-16 11:51:55 -0300

    wildfly imp

--- zipdin-sb-blank-application/src/main/java/com/zipdin/sbblank/ZipdinSpringBootBlankApplication.java
+++ zipdin-sb-blank-application/src/main/java/com/zipdin/sbblank/ZipdinSpringBootBlankApplication.java
@@ -2,12 +2,21 @@ package com.zipdin.sbblank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZipdinSpringBootBlankApplication {
+public class ZipdinSpringBootBlankApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipdinSpringBootBlankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipdinSpringBootBlankApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static final Class<ZipdinSpringBootBlankApplication> applicationClass = ZipdinSpringBootBlankApplication.class;
 
 }
---
+++ zipdin-sb-blank-interfaces/src/main/java/com/zipdin/sbblank/HelloController.java
@@ -0,0 +1,15 @@
+package com.zipdin.sbblank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "Hello World";
+    }
+}
