ba91417de1f66d33a2607163cc26c683525e143a
eduquirino08 <eduardo.quirino@zipdin.com.br>
2024-09-17 14:14:16 -0300

    removing warings and changing endpoint

--- zipdin-sb-blank-interfaces/src/main/java/com/zipdin/sbblank/HelloController.java
+++ zipdin-sb-blank-interfaces/src/main/java/com/zipdin/sbblank/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/list")
+    @GetMapping
     public String list() {
         return "Hello World";
     }
